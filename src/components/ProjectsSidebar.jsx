3c1a64c9646d611d79372490126e6d76e5d86364
anurag-babal <babalanurag2@gmail.com>
2024-06-16 22:37:49 +0530

    Frontend part complted

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,40 @@
+import Button from "./Button";
+
+export default function ProjectsSidebar({
+  onStartAddProject,
+  projects,
+  onSelectProject,
+  selectedProjectId,
+}) {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase text-stone-200 md:text-xl">
+        Your projects
+      </h2>
+      <div>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
+      </div>
+      <ul className="mt-8">
+        {projects.map((project) => {
+          let cssClasses =
+            "hover:text-stone-200 hover:bg-stone-800 text-left px-2 py-1 my-1 rounded-sm w-full";
+          if (project.id === selectedProjectId) {
+            cssClasses += " text-stome-200 bg-stone-800";
+          } else {
+            cssClasses += " text-stone-400";
+          }
+          return (
+            <li key={project.id}>
+              <button
+                onClick={() => onSelectProject(project.id)}
+                className={cssClasses}
+              >
+                {project.title}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+}
